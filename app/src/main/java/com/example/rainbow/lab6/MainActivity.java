ee83a79a0d16ffb3efeb7d2364d867c1083315a2
Rainbow412 <annie0760@163.com>
2016-11-06 22:46:42 +0800

    完成2.0 transact()方式通讯

--- app/src/main/java/com/example/rainbow/lab6/MainActivity.java
+++ app/src/main/java/com/example/rainbow/lab6/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Parcel;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,14 +23,23 @@ import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
     private Button play_pause_button, stop_button,quit_button;
     private TextView state, time1, time2;
     private ImageView imageAlbum;
     private MusicService ms;
     private SeekBar seekBar;
     private SimpleDateFormat time = new SimpleDateFormat("mm:ss");
-    private boolean isChanging=false;//互斥变量，防止定时器与SeekBar拖动时进度冲突
+    private boolean isChanging=false;//互斥变量，防止handler与SeekBar拖动时进度冲突
+    private boolean play_pause=false; //F:play T:pause
+    private boolean isPlaying = false;
+    //private boolean update_time = true;
     private ObjectAnimator animator;
+    private boolean flag = true; //图片旋转需要start还是resume
+    private int code,duration,currentPosition;
+    private Parcel data, reply;
+    private Message msg;
+    private IBinder mBinder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,54 +47,162 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findView();
         bindButton();
+        animatorInit();
         connection();
 
-        animator = ObjectAnimator.ofFloat(imageAlbum, "rotation", 0, 360);
-        animator.setDuration(10000);
-        animator.setRepeatCount(ObjectAnimator.INFINITE);
-        animator.setRepeatMode(ObjectAnimator.RESTART);
-        animator.setInterpolator(new LinearInterpolator());
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
-            @Override
-            public  void onProgressChanged(SeekBar arg0, int arg1, boolean arg2){
-                time1.setText(time.format(seekBar.getProgress()));
-                time2.setText(time.format(ms.mp.getDuration()-seekBar.getProgress()));
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar arg0){
-                //开始拖动进度条
-                isChanging=true;
-            }
-            @Override
-            public void onStopTrackingTouch(SeekBar arg0){
-                //停止拖动进度条
-                ms.mp.seekTo(seekBar.getProgress());
-                time1.setText(time.format(ms.mp.getCurrentPosition()));
-                time2.setText(time.format(ms.mp.getDuration()-ms.mp.getCurrentPosition()));
-                isChanging=false;
-            }
-
-        });
-
-
-
-
+        seekbarUpdate();
     }
 
-    Handler mHandler = new Handler();
+    final Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            super.handleMessage(msg);
+            switch (msg.what){
+                //更新currentPosition
+                case 200:
+                    if(mBinder!=null){
+                        code = 104;
+                        data = Parcel.obtain();
+                        reply = Parcel.obtain();
+                        try{
+                            mBinder.transact(code, data, reply, 0);
+                        }catch (RemoteException e){
+                            e.printStackTrace();
+                        }
+                        currentPosition = reply.readInt();
+                    }
+                    break;
+                //获取duration
+                case 201:
+                    if(mBinder!=null){
+                        code = 103;
+                        data = Parcel.obtain();
+                        reply = Parcel.obtain();
+                        try{
+                            mBinder.transact(code, data, reply, 0);
+                        }catch (RemoteException e){
+                            e.printStackTrace();
+                        }
+                        duration = reply.readInt();
+                    }
+                    break;
+                //play
+                case 202:
+                    if(mBinder!=null){
+                        code = 100;
+                        data = Parcel.obtain();
+                        reply = Parcel.obtain();
+                        try{
+                            mBinder.transact(code, data, reply, 0);
+                        }catch (RemoteException e){
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                //pause
+                case 203:
+                    if(mBinder!=null){
+                        code = 101;
+                        data = Parcel.obtain();
+                        reply = Parcel.obtain();
+                        try{
+                            mBinder.transact(code, data, reply, 0);
+                        }catch (RemoteException e){
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                //stop
+                case 204:
+                    if(mBinder!=null){
+                        code = 102;
+                        data = Parcel.obtain();
+                        reply = Parcel.obtain();
+                        try{
+                            mBinder.transact(code, data, reply, 0);
+                        }catch (RemoteException e){
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                //seekto
+                case 205:
+                    if(mBinder!=null){
+                        code = 105;
+                        data = Parcel.obtain();
+                        reply = Parcel.obtain();
+                        data.writeInt(seekBar.getProgress());
+                        try{
+                            mBinder.transact(code, data, reply, 0);
+                        }catch (RemoteException e){
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                //playing or not
+                case 206:
+                    if(mBinder!=null){
+                        code = 106;
+                        data = Parcel.obtain();
+                        reply = Parcel.obtain();
+                        try{
+                            mBinder.transact(code, data, reply, 0);
+                        }catch (RemoteException e){
+                            e.printStackTrace();
+                        }
+                        isPlaying = reply.readInt()==1?true:false;
+                        Log.d("isplay1", isPlaying==true?"t":"f");
+                    }
+                    break;
+            }
+        }
+    };
+    int t = 0;
     Runnable mRunnable = new Runnable() {
         @Override
         public void run() {
-            if(ms.mp != null){
-                if(isChanging==false){
-                    seekBar.setProgress(ms.mp.getCurrentPosition());
-                    //Log.d("Progress",""+ms.mp.getCurrentPosition());
-                    time1.setText(time.format(ms.mp.getCurrentPosition()));
-                    time2.setText(time.format(ms.mp.getDuration()-ms.mp.getCurrentPosition()));
-                }
-               mHandler.postDelayed(mRunnable, 100);
+            if(t<3||t==4) t++;
+            if(t==3){
+                //绑定需要一定时间
+                Log.d("mBinder==null", mBinder==null?"y":"n");
+                msg = new Message();
+                msg.what = 206; //isPlaying?
+                mHandler.sendMessage(msg);
+                t++;
             }
+            if(t==5){
+                //重新打开Activity要判断是否有音乐正在播放
+                Log.d("isplay2", isPlaying==true?"t":"f");
+                if(isPlaying==true){
+                    animator.start();
+                    Log.d("animator.start","y");
+                    state.setText("Playing");
+                    play_pause_button.setText("pause");
+                    flag=!flag;
+                    play_pause=!play_pause;
+                }
+                t++;
+            }
+            if(isChanging==false){
+                if(mBinder!=null){
+                        msg = new Message();
+                        msg.what = 200; //更新currentPosition
+                        mHandler.sendMessage(msg);
+                        seekBar.setProgress(currentPosition); //currentPosition
+                        //Log.d("Progress",""+currentPosition);
+
+                        msg = new Message();
+                        msg.what = 201; //更新duration
+                        mHandler.sendMessage(msg);
+                        seekBar.setMax(duration);
+                        //Log.d("Max",""+duration);
+
+                        time1.setText(time.format(currentPosition));
+                        time2.setText(time.format(duration-currentPosition));
+
+                }
+            }
+            mHandler.postDelayed(mRunnable, 100);
         }
     };
 
@@ -96,34 +216,98 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         seekBar = (SeekBar)findViewById(R.id.seekBar);
         imageAlbum = (ImageView)findViewById(R.id.image);
     }
-
     private void bindButton(){
         play_pause_button.setOnClickListener(this);
         stop_button.setOnClickListener(this);
         quit_button.setOnClickListener(this);
     }
-
+    private void animatorInit(){
+        animator = ObjectAnimator.ofFloat(imageAlbum, "rotation", 0, 360);
+        animator.setDuration(10000);
+        animator.setRepeatCount(ObjectAnimator.INFINITE);
+        animator.setRepeatMode(ObjectAnimator.RESTART);
+        animator.setInterpolator(new LinearInterpolator());
+    }
     private void connection(){
         Intent intent = new Intent(this, MusicService.class);
         bindService(intent, sc, BIND_AUTO_CREATE);
         mHandler.post(mRunnable);
 
     }
+    private void seekbarUpdate(){
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public  void onProgressChanged(SeekBar arg0, int arg1, boolean arg2){
+                time1.setText(time.format(seekBar.getProgress()));
+                time2.setText(time.format(duration-seekBar.getProgress()));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0){
+                //开始拖动进度条
+                isChanging=true;
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0){
+                //停止拖动进度条
+                isChanging=false;
+
+                msg = new Message();
+                msg.what = 205; //seekto
+                mHandler.sendMessage(msg);
+
+                msg = new Message();
+                msg.what = 200; //更新currentPosition
+                mHandler.sendMessage(msg);
+
+                time1.setText(time.format(currentPosition));
+                time2.setText(time.format(duration-currentPosition));
+            }
+        });
+    }
+
     @Override
     public void onClick(View v){
         switch (v.getId()){
             case R.id.play_pause:
-                ms.play(play_pause_button, state, animator);
-                seekBar.setMax(ms.mp.getDuration());
-                //Log.d("Max",""+ms.mp.getDuration());
+                if(play_pause==false){
+
+                    msg = new Message();
+                    msg.what = 202; //play
+                    mHandler.sendMessage(msg);
+
+                    play_pause_button.setText("pause");
+                    if(flag==true){
+                        animator.start();
+                        flag=false;
+                    }
+                    else
+                        animator.resume();
+                    state.setText("Playing");
+                    play_pause=true;
+                }
+                else{
+                    msg = new Message();
+                    msg.what = 203; //pause
+                    mHandler.sendMessage(msg);
+
+                    play_pause_button.setText("play");
+                    animator.pause();
+                    state.setText("Paused");
+                    play_pause=false;
+                }
                 break;
             case R.id.stop:
-                if(ms!=null)
-                    ms.stop();
-                    //seekBar.setProgress(0);
-                    state.setText("Stopped");
-                    play_pause_button.setText("Play");
-                    animator.cancel();
+                msg = new Message();
+                msg.what = 204; //stop
+                mHandler.sendMessage(msg);
+
+                state.setText("Stopped");
+                seekBar.setProgress(0);
+                currentPosition=0;
+                play_pause_button.setText("Play");
+                play_pause=false;
+                flag=true;
+                animator.cancel();
                 break;
             case R.id.quit:
                 mHandler.removeCallbacks(mRunnable);
@@ -134,7 +318,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } catch (Exception e){
                     e.printStackTrace();
                 }
-
                 break;
         }
 
@@ -143,14 +326,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ServiceConnection sc = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            ms = ((MusicService.MyBinder)service).getService();
+            //ms = ((MusicService.MyBinder)service).getService();
+            mBinder = (MusicService.MyBinder)service;
         }
-
         @Override
         public void onServiceDisconnected(ComponentName name) {
-            ms = null;
+            //ms = null;
+            mBinder = null;
         }
     };
+
+
+
 
 
 
--- app/src/main/java/com/example/rainbow/lab6/MusicService.java
+++ app/src/main/java/com/example/rainbow/lab6/MusicService.java
@@ -1,23 +1,57 @@
 package com.example.rainbow.lab6;
 
-import android.animation.ObjectAnimator;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Binder;
-import android.os.Environment;
 import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.TextView;
+
 
 public class MusicService extends Service{
 
-    private boolean flag = true; //图片旋转需要start还是resume
+
     public final IBinder binder = new MyBinder();
     public class MyBinder extends Binder{
-        MusicService getService(){
-            return MusicService.this;
+        @Override
+        protected  boolean onTransact(int code, Parcel data,
+                                      Parcel reply, int flags)
+                throws RemoteException{
+            switch (code){
+                case 100:
+                    play();
+                    break;
+                case 101:
+                    pause();
+                    break;
+                case 102:
+                    stop();
+                    break;
+                case 103:
+                    if(mp!=null){
+                        int duration = mp.getDuration();
+                        reply.writeInt(duration);
+                    }
+                    break;
+                case 104:
+                    if(mp!=null) {
+                        int currentPosition = mp.getCurrentPosition();
+                        reply.writeInt(currentPosition);
+                    }
+                    break;
+                case 105:
+                    int i = data.readInt();
+                    mp.seekTo(i);
+                    break;
+                case 106:
+                    int playing = mp.isPlaying()?1:0;
+                    Log.d("playing", Integer.toString(playing));
+                    reply.writeInt(playing);
+                    break;
+            }
+            return super.onTransact(code, data, reply, flags);
         }
     }
     @Override
@@ -29,15 +63,9 @@ public class MusicService extends Service{
         super.onCreate();
     }
     @Override
-    public void onDestroy(){
-        super.onDestroy();
-        if(mp != null){
-            mp.stop();
-            mp.release();
-        }
-    }
+    public void onDestroy(){ super.onDestroy(); }
 
-    public static MediaPlayer mp = new MediaPlayer();
+    private static MediaPlayer mp = new MediaPlayer();
     public MusicService(){
         try{
             //mp.setDataSource("/data/K.Will-Melt.mp3");
@@ -50,28 +78,21 @@ public class MusicService extends Service{
 
     }
 
-    public void play(Button b, TextView t, ObjectAnimator animator){
-        if(mp.isPlaying()){
-            mp.pause();
-            b.setText("play");
-            t.setText("Paused");
-            animator.pause();
-        } else{
+    private void play(){
+         if(!mp.isPlaying()){
             mp.start();
-            b.setText("pause");
-            t.setText("Playing");
-            if(flag==true){
-                animator.start();
-                flag = false;
-            }
-            else
-                animator.resume();
         }
     }
-    public void stop(){
+
+    private void pause(){
+        if(mp.isPlaying()){
+            mp.pause();
+        }
+    }
+    private void stop(){
         if(mp != null){
             mp.stop();
-            flag = true;
+//            flag = true;
             try {
                 mp.prepare();
                 mp.seekTo(0);
